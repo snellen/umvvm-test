9720224f5fc5c1182e1747597e6b4964c64b0b50
Silvan Nellen <silvan.nellen@gmail.com>
2020-04-08 09:09:13 +1000

    [uMvvm-test] Added documentation.

--- umvvm-test/src/main/java/ch/silvannellen/umvvm/test/common/TestBase.kt
+++ umvvm-test/src/main/java/ch/silvannellen/umvvm/test/common/TestBase.kt
@@ -7,7 +7,16 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 
+/**
+ * Use this class as base class for all unit tests of the umvvm use cases. It replaces the main dispatcher with a mocked version.
+ */
 open class TestBase {
+
+    /**
+     * This dispatcher replaces Dispatcher.Main. It exposes useful methods for testing, such
+     * as the ability to forward time to skip past any delay() calls in the code.
+     * See https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-test/kotlinx.coroutines.test/-test-coroutine-dispatcher/
+     */
     protected val mockedMainDispatcher = TestCoroutineDispatcher()
 
     @Before
--- umvvm-test/src/main/java/ch/silvannellen/umvvm/test/viewmodel/ViewModelTestBase.kt
+++ umvvm-test/src/main/java/ch/silvannellen/umvvm/test/viewmodel/ViewModelTestBase.kt
@@ -4,6 +4,10 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import ch.silvannellen.umvvm.test.common.TestBase
 import org.junit.Rule
 
+/**
+ * Use this class as base class for all unit tests of the umvvm view models. It contains a test rule
+ * that removes the dependency of LiveData on the UI (main) thread.
+ */
 open class ViewModelTestBase : TestBase() {
 
     @Rule
